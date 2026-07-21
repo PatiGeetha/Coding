0e120af3d6e2825f84a81f1a5192ea21087954b7
Geetha Pati <gethapati@gmail.com>
2026-07-21 21:49:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+def fizzBuzz(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return "FizzBuzz"
+    elif n % 3 == 0:
+        return "Fizz"
+    elif n % 5 == 0:
+        return "Buzz"
+    else:
+        return n
+
+print(fizzBuzz(n))
