cdcfc47e380a2e0ce5624d9e970efe3ce3ba6a6b
Geetha Pati <gethapati@gmail.com>
2026-08-01 23:29:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Method Overriding in Python/method-overriding-in-python.py
@@ -0,0 +1,14 @@
+class Employee:
+    def __init__(self,id,salary):
+        self.id=id 
+        self.salary = salary 
+    def get_info(self):
+        return f"EmployeeID:{id} Salary:{salary}" 
+        
+class SalesEmployee(Employee):
+    def __init__(self,id,salary,sales=0):
+        super().__init__(id,salary)  
+    def get_info(self):
+        return f"EmployeeID:{id} Salary:{salary} Sales:{sales}"
+
+        
